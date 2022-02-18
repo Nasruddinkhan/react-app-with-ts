14f7607267baa5b1c410ba412c54a7c85baf8bdf
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-02-18 22:34:23 +0530

    type script application with react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react';
+// import './App.css';
+import NewTodo from './components/NewTodo';
+import Todos from './components/Todos';
+import Todo from './models/todo';
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  // const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+  const addTodo = (value: string) => {
+    const newTodo = new Todo(value);
+    setTodos((prevTodo) => {
+      return prevTodo.concat(newTodo)
+    })
+
+  }
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTodo onAddTodo={addTodo} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
